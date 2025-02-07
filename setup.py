5ef33dbee4219f1043cbf51bb09c7feac4c3a020
kheil117 <109155996+kheil117@users.noreply.github.com>
2025-02-07 22:37:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setuptools.setup(
     package_data={
         "tinyman.v1": ["asc.json"],
         "tinyman.v2": ["amm_approval.map.json", "swap_router_approval.map.json"],
+        "tinyman.liquid_staking": ["structs.json"],
     },
     include_package_data=True,
 )
